688a6f6f8aee0a79c616ac03493aeb61fd11e996
David Mann <mannd@epstudiossoftware.com>
2013-09-29 18:55:09 -0400

    added Code.java

---
+++ src/org/epstudios/epcoding/Code.java
@@ -0,0 +1,48 @@
+package org.epstudios.epcoding;
+
+import android.widget.CheckBox;
+
+public class Code {
+	private final String code;
+	private final String shortDescription;
+	private final boolean isAddOn;
+	private CheckBox checkBox;
+
+	public Code(String code, String shortDescription, boolean isAddOn) {
+		this.code = code;
+		this.shortDescription = shortDescription;
+		this.isAddOn = isAddOn;
+	}
+
+	public void setCheckBox(CheckBox checkBox) {
+		this.checkBox = checkBox;
+	}
+
+	public void setChecked(boolean checked) {
+		checkBox.setChecked(checked);
+	}
+
+	public void setEnabled(boolean enabled) {
+		checkBox.setEnabled(enabled);
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public String getDescription() {
+		return shortDescription + " (" + code + ")";
+	}
+
+	public String getCodeFirstDescription() {
+		return code + " " + shortDescription;
+	}
+
+	public boolean isAddOn() {
+		return isAddOn;
+	}
+}
