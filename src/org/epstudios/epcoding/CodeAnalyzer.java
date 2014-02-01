86753e30c5b799702aa120b5bb7dbffc880784d1
David Mann <mannd@epstudiossoftware.com>
2014-02-01 23:03:08 +0100

    started on analysis of specific codesgroups

--- src/org/epstudios/epcoding/CodeAnalyzer.java
+++ src/org/epstudios/epcoding/CodeAnalyzer.java
@@ -1,5 +1,10 @@
 package org.epstudios.epcoding;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import android.content.Context;
 
 public class CodeAnalyzer {
@@ -9,6 +14,11 @@ public class CodeAnalyzer {
 	private final boolean moduleHasNoSecondaryCodes;
 	private final Context context;
 	private boolean verbose = false;
+
+	private final List<String> duplicateMappingCodes = Arrays.asList("93609",
+			"93613");
+
+	// enum here for error flags??
 
 	public CodeAnalyzer(final Code[] codes, final boolean noPrimaryCodes,
 			final boolean noSecondaryCodes,
@@ -47,7 +57,19 @@ public class CodeAnalyzer {
 			if (allAddOnCodes())
 				message += getMessage(ERROR, R.string.all_addons_error_message,
 						R.string.all_addons_verbose_error_message);
-			// other processing here
+			// check for forbidden code combos
+			// first make array of code numbers
+			String[] codeNumbers = new String[codes.length];
+			for (int i = 0; i < codes.length; ++i) {
+				if (codes[i] != null)
+					codeNumbers[i] = codes[i].getCodeNumber();
+			}
+			Set<String> numberSet = new HashSet<String>(
+					Arrays.asList(codeNumbers));
+			if (containsCodesNumbers(duplicateMappingCodes, numberSet))
+				message += getMessage(ERROR,
+						R.string.duplicate_mapping_codes_error_message,
+						R.string.duplicate_mapping_codes_verbose_error_message);
 			if (message.length() == 0) // no errors!
 				message = OK
 						+ context.getString(R.string.no_code_errors_message);
@@ -62,6 +84,11 @@ public class CodeAnalyzer {
 				allAddOns = false;
 		}
 		return allAddOns;
+	}
+
+	private boolean containsCodesNumbers(final List<String> badNumbers,
+			final Set<String> codeNumbers) {
+		return codeNumbers.containsAll(badNumbers);
 	}
 
 	private String getMessage(final String threat, final int brief,
