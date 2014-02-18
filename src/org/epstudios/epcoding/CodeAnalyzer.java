dcf1708c847146b22d8634e7f2ea269bb1de07ed
David Mann <mannd@epstudiossoftware.com>
2014-02-18 17:46:51 +0100

    made fixes, added copyright to Java source files

--- src/org/epstudios/epcoding/CodeAnalyzer.java
+++ src/org/epstudios/epcoding/CodeAnalyzer.java
@@ -1,3 +1,32 @@
+/*  
+Copyright (C) 2013, 2014 EP Studios, Inc.
+www.epstudiossoftware.com
+
+This file is part of EP Coding.
+
+    EP Coding is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    EP Coding is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with EP Coding.  If not, see <http://www.gnu.org/licenses/>.
+
+    Note also:
+
+    CPT copyright 2012 American Medical Association. All rights
+    reserved. CPT is a registered trademark of the American Medical
+    Association.
+
+    A limited number of CPT codes are used in this program under the Fair Use
+    doctrine of US Copyright Law.  See README.md for more information.
+ */
+
 package org.epstudios.epcoding;
 
 import java.util.ArrayList;
@@ -97,10 +126,9 @@ public class CodeAnalyzer {
 				.add(new CodeError(CodeError.WarningLevel.WARNING, Arrays
 						.asList("93650", "93600", "93619", "93620"),
 						"It is unclear if AV node ablation can be combined with EP testing codes."));
-		codeErrors
-				.add(new CodeError(CodeError.WarningLevel.ERROR, Arrays.asList(
-						"93656", "93621", "93462"),
-						"Code(s) selected are already included in 93656, AFB Ablation."));
+		codeErrors.add(new CodeError(CodeError.WarningLevel.ERROR, Arrays
+				.asList("93656", "93621", "93462"),
+				"Code(s) selected are already included in AFB Ablation."));
 		codeErrors
 				.add(new CodeError(
 						CodeError.WarningLevel.ERROR,
