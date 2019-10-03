153ada6e1940e12f25d488824242b1d95399c815
David Mann <mannd@epstudiossoftware.com>
2019-10-03 15:33:08 -0600

    Bump version to 1.10

--- app/src/main/java/org/epstudios/epcoding/About.java
+++ app/src/main/java/org/epstudios/epcoding/About.java
@@ -37,7 +37,7 @@ import java.util.Locale;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class About extends Activity {
-	private final static String VERSION = "1.9";
+	private final static String VERSION = "1.10";
 
 	private TextView versionTextView;
 
